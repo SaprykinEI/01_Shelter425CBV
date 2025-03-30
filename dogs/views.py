ce9c647af0034046bdc2c2a51fd945716def9532
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2025-03-30 12:57:04 +0300

    1. Написан breed_dogs_list_view, breed_list_view 2. Написан url для breed_list_view, breed_dogs_list_view, 3. Добавлены пустые шаблоны breeds.html & dogs.html

--- dogs/views.py
+++ dogs/views.py
@@ -8,3 +8,20 @@ def index_view(request):
         'title': "Питомник - Главное"
     }
     return render(request, 'dogs/index.html', context=context)
+
+def breeds_list_view(request):
+    context = {
+        'objects_list': Breed.objects.all(),
+        'title': "Питомник - Все наши породы"
+    }
+    return render(request, 'dogs/breeds.html',context=context)
+
+def breed_dogs_list_view(request, pk: int):
+    breed_object = Breed.objects.get(pk=pk)
+    context = {
+        'objects_list': Dog.objects.filter(breed_id=pk),
+        'title': f"Собаки породы - {breed_object.name}",
+        'breed_pk': breed_object.pk
+    }
+    return render(request, 'dogs/dogs.html', context=context)
+
