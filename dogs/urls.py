d8dab5c9bb9e52d9b7c0bf4c068d06197824f327
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2025-03-31 13:56:44 +0300

    1. Написаны dog_update_view, dog_delete_view; 2. Написаны urls для dog_update_view, dog_delete_view; 3. Добавлены пустые шаблоны update.html, delete.html

--- dogs/urls.py
+++ dogs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from dogs.apps import DogsConfig
 
 from dogs.views import (index_view, breeds_list_view, breed_dogs_list_view, dog_create_view, dogs_list_view,
-                        dog_detail_view)
+                        dog_detail_view, dog_update_view, dog_delete_view)
 
 app_name = DogsConfig.name
 
@@ -15,6 +15,8 @@ urlpatterns = [
     path('breeds/<int:pk>/dogs/', breed_dogs_list_view, name='breed_dogs'),
     path('dogs/', dogs_list_view, name='dogs_list'),
     path('dogs/create', dog_create_view, name='dog_create'),
-    path('dogs/detail/<int:pk>/', dog_detail_view, name='dog_detail')
+    path('dogs/detail/<int:pk>/', dog_detail_view, name='dog_detail'),
+    path('dogs/update/<int:pk>', dog_update_view, name='dog_update'),
+    path('dogs/delete/<int:pk>', dog_delete_view, name='dog_delete')
 
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -63,3 +63,29 @@ def dog_detail_view(request, pk):
     }
     return render(request, 'dogs/detail.html', context=context)
 
+def dog_update_view(request, pk):
+    """Редактировать данные о собаке"""
+    dog_object = get_object_or_404(Dog, pk=pk)
+    if request.method == 'POST':
+        form = DogForm(request.POST, request.FILES, instance=dog_object)
+        if form.is_valid():
+            dog_object = form.save()
+            dog_object.save()
+            return HttpResponseRedirect(reverse('dogs:dog_detail', args={pk: pk}))
+    context = {
+        'object': dog_object,
+        'title': 'Изменить собаку',
+        'form': DogForm(instance=dog_object),
+    }
+    return render(request, 'dogs/update.html', context=context)
+
+def dog_delete_view(request, pk):
+    dog_object = get_object_or_404(Dog, pk=pk)
+    if request.method == 'POST':
+        dog_object.delete()
+        return HttpResponseRedirect(reverse('dogs:dogs_list'))
+    context = {
+        'object': dog_object,
+        'title': "Удалить собаку"
+    }
+    return render(request, 'dogs/delete.html', context=context)
