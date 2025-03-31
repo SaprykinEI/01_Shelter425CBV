afff2ff03a44139d3a52112518926c5831b059cf
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2025-03-31 11:45:58 +0300

    1. Написан dog_create_view, dogs_list_view 2. Написаны urls для dog_create_view, dogs_list_view 3. Добавлены пустые шаблон create.html

--- dogs/views.py
+++ dogs/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from dogs.models import Breed, Dog
+from dogs.forms import DogForm
 
 def index_view(request):
+    """Функция отвечает за отображение главной страницы питомника."""
     context = {
         'objects_list': Breed.objects.all()[:3],
         'title': "Питомник - Главное"
@@ -10,6 +14,7 @@ def index_view(request):
     return render(request, 'dogs/index.html', context=context)
 
 def breeds_list_view(request):
+    """Функция отображает список всех пород собак, доступных в питомнике."""
     context = {
         'objects_list': Breed.objects.all(),
         'title': "Питомник - Все наши породы"
@@ -17,6 +22,7 @@ def breeds_list_view(request):
     return render(request, 'dogs/breeds.html',context=context)
 
 def breed_dogs_list_view(request, pk: int):
+    """Отображение списка собак определенной породы."""
     breed_object = Breed.objects.get(pk=pk)
     context = {
         'objects_list': Dog.objects.filter(breed_id=pk),
@@ -24,4 +30,27 @@ def breed_dogs_list_view(request, pk: int):
         'breed_pk': breed_object.pk
     }
     return render(request, 'dogs/dogs.html', context=context)
+
+#Create Read Update Delete(CRUD)
+
+def dogs_list_view(request):
+    """Вывод списка всех собак на отдельной странице"""
+    context = {
+        'objects_list': Dog.objects.all(),
+        'title': f"Все наши собаки",
+    }
+    return render(request, 'dogs/dogs.html', context=context)
+
+def dog_create_view(request):
+    """ Создание новой собаки в базе данных через форму"""
+    if request.method == 'POST':
+        form = DogForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('dogs:dogs_list'))
+    context = {
+        'title': 'Добавить собаку',
+        'form': DogForm
+    }
+    return render(request, 'dogs/create.html', context=context)
 
