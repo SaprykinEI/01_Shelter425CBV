9643be4c4735534fca5683d80959358f7d77f408
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2025-04-06 12:14:19 +0300

    Расширена модель пользователя проведены миграции

--- users/models.py
+++ users/models.py
@@ -7,8 +7,11 @@ NULLABLE = {'blank': True, 'null': True}
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Email')
+    first_name = models.CharField(max_length=150, verbose_name='Имя', default='Anonymous')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия', default='Anonymous')
     phone = models.CharField(max_length=35, unique=True, verbose_name='Телефон', **NULLABLE)
     telegram = models.CharField(max_length=150, unique=True, verbose_name='Телеграм', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Автар', **NULLABLE)
     is_active = models.BooleanField(default=True, verbose_name='Активность')
 
     USERNAME_FIELD = 'email'
