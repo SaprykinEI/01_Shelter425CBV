423a9704611ecb47080b0120be36c358a0c9c3bf
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2025-04-22 10:10:30 +0300

    1. Обновлена команда создания БД 2. Обновлены настройки для пользователя

--- users/management/commands/ccdb.py
+++ users/management/commands/ccdb.py
@@ -15,10 +15,13 @@ class Command(BaseCommand):
 
         try:
             conn = pyodbc.connect(ConnectionString)
-            conn.autocommit = True
-            # conn.execute(fr"DROP DATABASE {DATABASE};")
-            conn.execute(fr'CREATE DATABASE {DATABASE};')
         except pyodbc.ProgrammingError as ex:
             print(ex)
         else:
-            print(f"База данных {DATABASE} успешно создана")
+            conn.autocommit = True
+            try:
+                conn.execute(fr'CREATE DATABASE {DATABASE};')
+            except pyodbc.ProgrammingError as ex:
+                print(ex)
+            else:
+                print(f"База данных {DATABASE} успешно создана")
