5707527595f2ef51f2b324506bf61e4132abcd64
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2025-05-12 12:02:06 +0300

    1. Добавлена CRUD reviews/views

--- reviews/views.py
+++ reviews/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from django.core.exceptions import PermissionDenied
 
 from reviews.models import Review
-from users.models import User
+from users.models import User, UserRoles
 from reviews.forms import ReviewAdminForm
 
 
@@ -30,3 +30,44 @@ class ReviewDeactivatedListView(ListView):
     def get_queryset(self):
         return super().get_queryset().filter(sign_of_review=False)
 
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    form_class = ReviewAdminForm
+    template_name = 'reviews/create.html'
+    extra_context = {
+        'title': 'Добавить отзыв'
+    }
+
+class ReviewDetailView(DetailView):
+    model = Review
+    template_name = 'reviews/detail.html'
+    extra_context = {
+        'title': "Просмотр отзыва"
+    }
+
+
+class ReviewUpdateView(LoginRequiredMixin, UpdateView):
+    model = Review
+    form_class = ReviewAdminForm
+    template_name = 'reviews/update.html'
+    extra_context = {
+        'title': 'Изменить отзыв'
+    }
+
+    def get_object(self, queryset=None):
+        object_ = super().get_object(queryset=queryset)
+        if object_.author != self.request.user and self.request.user.role not in [UserRoles.ADMIN, UserRoles.MODERATOR]:
+            raise PermissionDenied()
+        return object_
+    def get_success_url(self):
+        return reverse('review:review_detail', args=[self.kwargs.get('slug')])
+
+
+class ReviewDeleteView(PermissionRequiredMixin, DeleteView):
+    model = Review
+    template_name = 'reviews/delete.html'
+    permission_required = 'reviews.delete_review'
+
+    def get_success_url(self):
+        return reverse('reviews:reviews_list')
